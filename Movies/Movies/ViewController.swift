682358b885d1cce119f0c644cdb50320b47aa576
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-12-15 14:01:02 +0200

    Initial commit

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Deniz Mehmed on 15.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
