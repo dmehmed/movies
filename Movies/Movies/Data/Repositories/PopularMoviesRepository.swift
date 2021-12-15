71c02c673f2e7e0c6822d41aea566769fb21a2a4
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-12-15 16:56:50 +0200

    Pair programming -> concrete implementation of popular movies repository

---
+++ Movies/Movies/Data/Repositories/PopularMoviesRepository.swift
@@ -0,0 +1,27 @@
+//
+//  PopularMoviesRepository.swift
+//  Movies
+//
+//  Created by Deniz Mehmed on 15.12.21.
+//
+
+import Foundation
+
+public class PopularMoviesRepository: MovieListRepository {
+    
+    private let service: MovieListServiceProtocol
+    
+    public init(_ service: MovieListServiceProtocol) {
+        self.service = service
+    }
+    
+    public func fetchPopularMovies(_ completion: @escaping (MovieList?) -> Void) {
+        self.service.getPopularMovies { result in
+            print(result)
+            // parse func in this file filter etc funcs
+            
+            completion(result)
+        }
+    }
+    
+}
