fd84bd01afe031072f3457c4b9bb51c87c37f0f1
Deniz A. Mehmed <denizmehmed97@gmail.com>
2021-12-20 09:50:17 +0200

    Get top rated movies in service

--- Movies/Movies/Data/Services/MovieListService.swift
+++ Movies/Movies/Data/Services/MovieListService.swift
@@ -9,12 +9,17 @@ import Foundation
 
 public protocol MovieListServiceProtocol {
     func getPopularMovies(_ completion: @escaping (MovieList?) -> Void)
+    func getTopRatedMovies(_ completion: @escaping (MovieList?) -> Void)
 }
 
 public class MovieListService: Service, MovieListServiceProtocol {
     
     public func getPopularMovies(_ completion: @escaping (MovieList?) -> Void) {
         network.perform(PopularMoviesRequest(), with: completion)
+    }
+    
+    public func getTopRatedMovies(_ completion: @escaping (MovieList?) -> Void) {
+        network.perform(TopRatedMoviesRequest(), with: completion)
     }
     
 }
